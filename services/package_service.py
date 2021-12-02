9ba76c5b61cfbad9f6d5444a49ce4934c5ef5f2c
Andy <andy.parinas@gmail.com>
2021-12-02 19:55:38 +1000

    Live data in homepage

--- services/package_service.py
+++ services/package_service.py
@@ -1,20 +1,45 @@
 from datetime import datetime
 from typing import List, Optional
 
+from sqlalchemy import orm
+
 from models.package import Package
 from models.release import Release
 
+from infrastructure import db_session
+
 
 def release_count():
-    return 0
+    session = db_session.create_session()
+
+    try:
+        return session.query(Release).count()
+    finally:
+        session.close()
 
 
 def package_count():
-    return 0
+    session = db_session.create_session()
+
+    try:
+        return session.query(Package).count()
+    finally:
+        session.close()
 
 
-def latest_releases(limit: int = 10) -> List:
-    return []
+def latest_releases(limit: int = 10) -> List[Package]:
+    session = db_session.create_session()
+
+    try:
+        releases = session.query(Release) \
+            .options(orm.joinedload(Release.package)) \
+            .order_by(Release.created_date.desc()) \
+            .limit(limit) \
+            .all()
+    finally:
+        session.close()
+
+    return list({r.package for r in releases})
 
 
 def get_package_by_id(package_name: str) -> Optional[Package]:
--- services/user_service.py
+++ services/user_service.py
@@ -1,14 +1,21 @@
-
+from infrastructure import db_session
 from models.user import User
+
 
 def login_user(email: str, password: str):
     # TODO: check if email and password match for user
 
     return User('Andy', email, password)
 
+
 def user_count() -> int:
-    pass
+    session = db_session.create_session()
+
+    try:
+        return session.query(User).count()
+    finally:
+        session.close()
+
 
 def create_account(name: str, email: str, password: str):
     return User(name, email, password)
-
